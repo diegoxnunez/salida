8c2f9779fa49c6b9e6dd3fd11556c1bafcb1c985
diegoxnunez <129469982+diegoxnunez@users.noreply.github.com>
2023-08-03 08:00:47 -0400

    Revert "mesa commit"
    This reverts commit e7f387b4666ed543148241b6f6fd1474f1932b11.

--- prueba/src/main/java/cl/awakelab/prueba/model/persistence/repository/MesaRepository.java
+++
@@ -1,9 +0,0 @@
-package cl.awakelab.prueba.model.persistence.repository;
-
-import cl.awakelab.prueba.model.entity.MesaEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MesaRepository extends JpaRepository<MesaEntity, Integer>{
-
-
-}
